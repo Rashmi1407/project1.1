5138bf3823280205704adf837ccea7dc135dda60
Rashmi1407 <139221826+Rashmi1407@users.noreply.github.com>
2023-08-08 11:14:59 +0530

    Create file1.c

---
+++ file1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main(){
+    int sum=0;
+int a[10]={1,2,3,4,5};
+for(int i=0;i<10;i++){
+    sum=sum+a[i];
+}    
+printf("sum=%d",sum); 
+    
+}
+system("./increment_version.sh");
